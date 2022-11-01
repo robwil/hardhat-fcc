56bbf01b860645e9c82fe48581c9c3d8c95b9fe3
Rob Williams <contact@robwilliams.me>
2022-11-01 16:45:22 -0400

    gas reporter and solidity test coverage

--- hardhat-simple-storage/hardhat.config.ts
+++ hardhat-simple-storage/hardhat.config.ts
@@ -3,11 +3,14 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from "dotenv";
 import "./tasks/block-number";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
 dotenv.config({ path: __dirname + "/.env" });
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL!;
 const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY!;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY!;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY!;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
@@ -25,6 +28,13 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: false,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
   },
 };
 
